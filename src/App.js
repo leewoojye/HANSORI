1f48e4a6b70bca63bda9e45560fd25723c5dedcd
이유진 <nnjin987@gmail.com>
2022-02-12 13:56:00 +0900

    lyj

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Profile from "./components/Profile";
 import Test from "./components/Test";
 import Board from "./components/Board";
 import Pungmul from "./components/Pungmul";
+import lyj from "./components/lyj";
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
       <Route path="/test" component={Test} />
       <Route path="/board" component={Board} />
       <Route path="/pungmul" component={Pungmul} />
+      <Route path="/lyj" component={lyj} />
       <Footer />
     </>
   );
---
+++ src/components/lyj.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function lyj() {
+  return (
+    <div
+      className="contents"
+      style={{ height: "100vh", paddingTop: "50vh", backgroundColor: "black" }}
+    >
+      이유진
+    </div>
+  );
+}
+
+export default lyj;
