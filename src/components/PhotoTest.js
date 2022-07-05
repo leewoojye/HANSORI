0c5b1a501643c2707452690ad9151cee6d18bbe5
changjini32 <84197474+changjini32@users.noreply.github.com>
2022-07-05 16:02:31 +0900

    메인화면 수정
    화면 크기 맞게 height 조절

--- src/components/PhotoTest.js
+++ src/components/PhotoTest.js
@@ -1,26 +1,17 @@
-import React from "react";
+const { google } = require("googleapis");
+const Photos = require("googlephotos");
 
-async function PhotoTest() {
-  // const { google } = require("googleapis");
-  // const Photos = require("googlephotos");
-  // const photos = new Photos("AIzaSyDcnoFRHVqc-Hn9jSIp2fWgXJi2bxLuhpM");
+const photos = new Photos("AIzaSyDcnoFRHVqc-Hn9jSIp2fWgXJi2bxLuhpM");
 
-  // const oauth2Client = new google.auth.OAuth2(
-  //   "835389304145-pll2ngg9i01e00vajc7svevd555h1g6b.apps.googleusercontent.com",
-  //   "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",
-  //   "https://hansori.net"
-  // );
+const oauth2Client = new google.auth.OAuth2(
+  "835389304145-pll2ngg9i01e00vajc7svevd555h1g6b.apps.googleusercontent.com",
+  "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",
+  "https://hansori.net"
+);
 
-  // const scopes = [Photos.Scopes.READ_ONLY, Photos.Scopes.SHARING];
+const scopes = [Photos.Scopes.READ_ONLY, Photos.Scopes.SHARING];
 
-  // const url = oauth2Client.generateAuthUrl({
-  //   access_type: "offline",
-  //   scope: scopes,
-  // });
-
-  // const { tokens } = await oauth2Client.getToken(code);
-
-  return <div>asdfasddddddddddfasdfsadfasd</div>;
-}
-
-export default PhotoTest;
+const url = oauth2Client.generateAuthUrl({
+  access_type: "offline",
+  scope: scopes,
+});
