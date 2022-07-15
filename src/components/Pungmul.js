5dc9d086e51c0de9526e5c1682c16fd2d2722fb1
GGyong <84078044+jgy1231@users.noreply.github.com>
2022-07-15 13:45:13 +0900

    풍물 소리 넣기
    .

--- src/components/Pungmul.js
+++ src/components/Pungmul.js
@@ -5,6 +5,12 @@ import inst3 from "../asset/instruments/inst3.png";
 import inst4 from "../asset/instruments/inst4.png";
 import inst5 from "../asset/instruments/inst5.png";
 
+import sound1 from '../asset/audio/metal.mp3'
+import sound2 from '../asset/audio/janggu.mp3'
+import sound3 from '../asset/audio/drum.mp3'
+import sound4 from '../asset/audio/jing.mp3'
+import sound5 from '../asset/audio/sogo.mp3'
+
 const index = [0, 1, 2, 3, 4];
 function shuffle(array) {
   array.sort(() => Math.random() - 0.5);
@@ -18,6 +24,14 @@ const _inst = [
   [inst4, "징"],
   [inst5, "소고"],
 ];
+
+const _sound = [
+  new Audio(sound1),
+  new Audio(sound2),
+  new Audio(sound3),
+  new Audio(sound4),
+  new Audio(sound5),
+]
 
 const size = [
   50 * Math.random() + 150,
@@ -55,6 +69,7 @@ function Pungmul() {
             src={_inst[index[0]][0]}
             alt=""
             style={{ height: size[1], filter: "invert(100%)" }}
+            onClick={() => _sound[index[0]].play()}
           />
           <div className="text">{_inst[index[0]][1]}</div>
         </div>
@@ -72,6 +87,7 @@ function Pungmul() {
             src={_inst[index[1]][0]}
             alt=""
             style={{ height: size[1], filter: "invert(100%)" }}
+            onClick={() => _sound[index[1]].play()}
           />
           <div className="text">{_inst[index[1]][1]}</div>
         </div>
@@ -89,6 +105,7 @@ function Pungmul() {
             src={_inst[index[2]][0]}
             alt=""
             style={{ height: size[2], filter: "invert(100%)" }}
+            onClick={() => _sound[index[2]].play()}
           />
           <div className="text">{_inst[index[2]][1]}</div>
         </div>
@@ -106,6 +123,7 @@ function Pungmul() {
             src={_inst[index[3]][0]}
             alt=""
             style={{ height: size[3], filter: "invert(100%)" }}
+            onClick={() => _sound[index[3]].play()}
           />
           <div className="text">{_inst[index[3]][1]}</div>
         </div>
@@ -123,6 +141,7 @@ function Pungmul() {
             src={_inst[index[4]][0]}
             alt=""
             style={{ height: size[4], filter: "invert(100%)" }}
+            onClick={() => _sound[index[4]].play()}
           />
           <div className="text">{_inst[index[4]][1]}</div>
         </div>
